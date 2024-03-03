e42261ad8f3a76d413ed03dfe5ff2a803a763f83
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-03-03 22:04:04 +0100

    add: layout for the upper section of the page

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ import { Scene, PerspectiveCamera, WebGLRenderer } from "three";
 
 const scene = new Scene();
 
-const camera = new PerspectiveCamera(70, innerWidth/innerHeight);
+const width = innerWidth;
+const height = 768;
+
+const camera = new PerspectiveCamera(70, width/height);
 camera.position.set(0, 0, 4);
 camera.lookAt(0, 0, 0);
 
 const canvas = document.querySelector('canvas');
 const graphic = new WebGLRenderer({canvas});
+
+graphic.setSize(width, height);
+
 graphic.render(scene, camera);
 
