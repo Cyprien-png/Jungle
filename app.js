954cd166618a6c1d4ab2679263996392fd66cd4c
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-03-03 22:17:06 +0100

    add: basic mesh to the scene

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { Scene, PerspectiveCamera, WebGLRenderer } from "three";
+import { Scene, PerspectiveCamera, WebGLRenderer, Mesh, MeshPhongMaterial, PointLight, BoxGeometry } from "three";
 
 const scene = new Scene();
 
@@ -8,6 +8,19 @@ const height = 768;
 const camera = new PerspectiveCamera(70, width/height);
 camera.position.set(0, 0, 4);
 camera.lookAt(0, 0, 0);
+
+
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshPhongMaterial();
+const cube = new Mesh(geometry, material);
+
+const light = new PointLight();
+light.position.set(0, 2, 1);
+light.lookAt(0, 0, 0);
+
+scene.add(cube);
+scene.add(light);
+
 
 const canvas = document.querySelector('canvas');
 const graphic = new WebGLRenderer({canvas});
